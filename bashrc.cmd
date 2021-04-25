b6ce80152582c14dc7fbc5c2d88d0dce75759274
LUKE DICKERSON <lucius.dickerson@gmail.com>
2021-04-24 17:28:38 -0700

    updated stuff

--- bashrc.cmd
+++ bashrc.cmd
@@ -1,14 +1,16 @@
 @echo off
 
-
-
 doskey ls=dir $*
 doskey cp=copy $*
 doskey cat=type $*
 doskey pwd=cd
-doskey touch=python C:\Users\ld085a\linux_terminal_commands.py touch $*
-doskey rm=python C:\Users\ld085a\linux_terminal_commands.py rm $*
-doskey mv=python C:\Users\ld085a\linux_terminal_commands.py mv $*
+
+rem Get this files path
+rem %~f0 will return the full pathname (e.g. W:\scripts\mybatch.cmd
+rem source: https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
+doskey touch=python %~dp1\linux_terminal_commands.py touch $*
+doskey rm=python %~dp1\linux_terminal_commands.py rm $*
+doskey mv=python %~dp1\linux_terminal_commands.py mv $*
 
 rem tree doesn't show files by default, /f shows the files
 rem aliasing it in this way makes tree show the current
